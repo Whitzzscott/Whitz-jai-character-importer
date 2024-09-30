904cd6e5321c47654506507bcb134f7da2604993
whitzscott <samytplayz197@gmail.com>
2024-09-30 18:12:07 +0800

    Add files via upload

---
+++ extension/type.js
@@ -0,0 +1,26 @@
+/**
+ * @typedef {Object} UniCharacter
+ * @property {string} char_name
+ * @property {string} char_persona
+ * @property {string} char_greeting
+ * @property {string} world_scenario
+ * @property {string} example_dialogue
+ * @property {string} name
+ * @property {string} description
+ * @property {string} first_mes
+ * @property {string} scenario
+ * @property {string} mes_example
+ * @property {string} personality
+ * @property {Metadata} metadata
+ */
+
+/**
+ * @typedef {Object} JAICharacter
+ * @property {string} name
+ * @property {string} description
+ * @property {string} personality
+ * @property {string} scenario
+ * @property {string} example_dialogs
+ * @property {string} first_message
+ * @property {boolean} showDefinition
+ */
